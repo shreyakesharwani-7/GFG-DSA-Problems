55f7335a61a539ecdb86f95805157259444f591b
Shreya Kesharwani <shreyakesharwani5524@gmail.com>
2025-08-15 10:15:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print With Separator/print-with-separator.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class Solution {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        String sep = sc.nextLine();
+        // Print in the required format and a new line
+        System.out.println(a+sep+b);
+    }
+}
